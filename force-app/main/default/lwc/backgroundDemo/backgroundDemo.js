ecdd84c2e8dd9894ce712a20d5f7fdec21544cc7
brianmfear <brian.m.fear@gmail.com>
2021-11-10 13:36:32 -0700

    remove unnecessary getter/setter

--- force-app/main/default/lwc/backgroundDemo/backgroundDemo.js
+++ force-app/main/default/lwc/backgroundDemo/backgroundDemo.js
@@ -1,12 +1,8 @@
 import { api, LightningElement } from 'lwc';
 
 export default class BackgroundDemo extends LightningElement {
-    _background;
-    @api set background(value) {
-        this._background = value;
-    }
-    get background() { return this._background; }
+    @api background;
     renderedCallback() {
-        this.template.querySelector('.background')?.style.setProperty('--background', `url(${this._background}`);
+        this.template.querySelector('.background')?.style.setProperty('--background', `url(${this.background}`);
     }
 }
